b007223211dc157f8f91e3ce76623958ba81dbe6
Kyle Gorman <kylebgorman@gmail.com>
2020-02-20 19:36:42 -0500

    Uses enable-aliases

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,8 @@
 ./configure \
    --prefix="${PREFIX}" \
-   --enable-compact-fsts \
    --enable-compress \
-   --enable-const-fsts \
-   --enable-far \
-   --enable-linear-fsts \
-   --enable-lookahead-fsts \
-   --enable-mpdt \
-   --enable-ngram-fsts \
-   --enable-pdt \
+   --enable-fsts \
+   --enable-grm \
    --enable-special
 make -j"${CPU_COUNT}" check
 make -j"${CPU_COUNT}" install
