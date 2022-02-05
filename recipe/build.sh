ab35ae3a21ab18d31a0129494b606a6f58da5de3
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2022-02-05 08:22:13 -0800

    Add build for osx_arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,11 +3,18 @@
 # https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
 export CXXFLAGS="$(echo "${CXXFLAGS}" | sed -E 's@-std=c\+\+[^ ]+@@g') -D_LIBCPP_DISABLE_AVAILABILITY"
 
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* .
+
 ./configure \
    --prefix="${PREFIX}" \
    --enable-compress \
    --enable-fsts \
    --enable-grm \
    --enable-special
-make -j"${CPU_COUNT}" check
+   
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
+  make -j"${CPU_COUNT}" check
+fi
+
 make -j"${CPU_COUNT}" install
