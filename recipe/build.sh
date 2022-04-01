34fabce0a552836471b75c2475640323b11693c6
H. Vetinari <h.vetinari@gmx.com>
2022-04-01 12:38:48 +1100

    add bld.bat using clang autotools

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,13 @@
+#!/bin/bash
+set -ex
+
 # Removes -std=c++XX from build ${CXXFLAGS}.
 # Ignores irrelevant Clang availability annotations on MacOS: 
 # https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
 export CXXFLAGS="$(echo "${CXXFLAGS}" | sed -E 's@-std=c\+\+[^ ]+@@g') -D_LIBCPP_DISABLE_AVAILABILITY"
 
 # Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* .
+cp $BUILD_PREFIX/share/gnuconfig/config.* . || true
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]]; then
   OPENFST_CROSS_COMPILATION_CONFIGURE_OPTS="--build=${BUILD} --host=${HOST}"
@@ -17,7 +20,10 @@ fi
    --enable-compress \
    --enable-fsts \
    --enable-grm \
-   --enable-special ${OPENFST_CROSS_COMPILATION_CONFIGURE_OPTS}
+   --enable-special \
+   ${OPENFST_CROSS_COMPILATION_CONFIGURE_OPTS}
+
+[[ "$target_platform" == "win-64" ]] && patch_libtool
 
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
   make -j"${CPU_COUNT}" check
