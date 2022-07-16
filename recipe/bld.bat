a57d0e066142de84858bde0bb4a5c8c296bdbc3f
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2022-07-16 12:06:59 -0700

    Update windows build to use CMake

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,19 @@
-call %BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat
+
+mkdir build
+cd build
+
+set "LIBRARY_PREFIX=%LIBRARY_PREFIX:\=/%"
+
+cmake -GNinja ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+    -DBUILD_SHARED_LIBS=ON ^
+    ..
+if %ERRORLEVEL% neq 0 exit 1
+
+cmake --build . --verbose --config Release -- -v -j 1
+if %ERRORLEVEL% neq 0 exit 1
+
+cmake --install . --verbose --config Release
 if %ERRORLEVEL% neq 0 exit 1
